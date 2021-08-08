bcb0b8cc276a452a9d5eacef6b742cd9f7a86ab8
izaccavalheiro <izac@izaccavalheiro.com>
2021-08-08 13:04:13 -0300

    Update Paragraph component style

--- src/presentation/components/Paragraph/styles.js
+++ src/presentation/components/Paragraph/styles.js
@@ -3,7 +3,6 @@ import styled from 'styled-components'
 import { defaultClasses } from '../../styles'
 
 export const StyledParagraph = styled.p`
-  margin-bottom: 1rem;
   font-size: 1rem;
   color: #000;
   display: flex;
