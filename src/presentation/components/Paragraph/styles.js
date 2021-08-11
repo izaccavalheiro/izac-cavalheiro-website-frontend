c950fddd6f8750a9c8cf6068661bba2a95eaf187
izaccavalheiro <izac@izaccavalheiro.com>
2021-08-10 22:15:49 -0300

    Update paragraph component styles

--- src/presentation/components/Paragraph/styles.js
+++ src/presentation/components/Paragraph/styles.js
@@ -6,6 +6,11 @@ export const StyledParagraph = styled.p`
   font-size: 1.5rem;
   color: #000;
   display: flex;
+  line-height: 1.5rem;
 
   ${defaultClasses}
+
+  &:last-child {
+    margin-bottom: 0;
+  }
 `
