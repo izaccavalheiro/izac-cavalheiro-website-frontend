67835629e75c9f23e00a9125483c07955655d59a
izaccavalheiro <izac@izaccavalheiro.com>
2021-08-10 22:14:27 -0300

    Update social bar component styles

--- src/presentation/modules/Socialbar/styles.js
+++ src/presentation/modules/Socialbar/styles.js
@@ -5,38 +5,32 @@ import {
 } from '../../components'
 
 export const Wrapper = styled(List)`
-  @media(min-width: 0rem) {
-    flex-direction: row;
-    flex-wrap: wrap;
+  flex-direction: row;
+  flex-wrap: wrap;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: .5rem;
+  max-width: 100%;
+  list-style: inside none;
+  
+  > li {
+    flex: none;
     display: flex;
     align-items: center;
     justify-content: center;
-    gap: 1.5rem;
-    max-width: 100%;
-    list-style: inside none;
-    
-    > li {
-      flex: 1;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      max-height: 1.2rem;
-      min-height: 1.2rem;
-      max-width: 1.2rem;
-      min-width: 1.2rem;
 
-      > ic-socialnetwork {
-        min-width: 100%;
-        max-width: 100%;
-      }
+    > ic-socialnetwork {
+      min-width: 100%;
+      max-width: 100%;
     }
   }
 
-  @media(min-width: 20rem) {
-    
+  @media(min-width: 30rem) {
+    gap: .75rem;
   }
 
   @media(min-width: 60rem) {
-    
+    gap: 1rem;
   }
 `
