f604b643977e5dd3b28a46dcef756f9ab7b4a60e
izaccavalheiro <izac@izaccavalheiro.com>
2021-08-10 22:15:08 -0300

    Update h1 component styles

--- src/presentation/components/H1/styles.js
+++ src/presentation/components/H1/styles.js
@@ -4,6 +4,7 @@ import { defaultClasses } from '../../styles'
 
 export const StyledH1 = styled.h1`
   font-size: 2rem;
+  line-height: 2rem;
   color: #000;
   text-transform: uppercase;
   margin-bottom: 1rem;
