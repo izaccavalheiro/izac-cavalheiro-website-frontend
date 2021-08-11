0038c52632e2daff3f2bdca05db9ddd626ed9b94
izaccavalheiro <izac@izaccavalheiro.com>
2021-08-10 23:29:40 -0300

    Update gatsby-config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "Izac's Website"
+    siteUrl: "https://izaccavalheiro.com",
+    title: "Izac Cavalheiro - Software Engineer"
   },
   plugins: [
     "gatsby-plugin-styled-components",
