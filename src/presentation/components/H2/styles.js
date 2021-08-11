56f50b5c2a56fe5aff2c4534b9abab48951a777a
izaccavalheiro <izac@izaccavalheiro.com>
2021-08-10 22:15:21 -0300

    Update h2 component styles

--- src/presentation/components/H2/styles.js
+++ src/presentation/components/H2/styles.js
@@ -4,6 +4,7 @@ import { defaultClasses } from '../../styles'
 
 export const StyledH2 = styled.h2`
   font-size: 1.5rem;
+  line-height: 1.5rem;
   color: #000;
   text-transform: uppercase;
   margin-bottom: 1rem;
