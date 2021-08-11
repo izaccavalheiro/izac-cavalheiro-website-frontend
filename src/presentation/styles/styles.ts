338c607c10098e0958a66f3d12b3865540c943c8
izaccavalheiro <izac@izaccavalheiro.com>
2021-08-10 22:17:14 -0300

    Update default styles

--- src/presentation/styles/styles.ts
+++ src/presentation/styles/styles.ts
@@ -18,6 +18,27 @@ export const fadeIn = css`
   }
 `
 
+export const slideUp = css`
+  animation: slideUp .5s;
+  animation-delay: .25s;
+  animation-fill-mode: forwards;
+  opacity: 0;
+  transition: all .5s ease-in;
+  position: relative;
+
+  @keyframes slideUp {
+    0% {
+      top: 10px;
+      opacity: 0;
+    }
+
+    100% {
+      top: 0px;
+      opacity: 1;
+    }
+  }
+`
+
 export const defaultClasses = css`
   &.text-align-center {
     text-align: center;
