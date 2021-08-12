c4228d3590dfb4dcbf2a43cab6a15f9abefcef6c
izaccavalheiro <izac@izaccavalheiro.com>
2021-08-12 00:42:19 -0300

    Add gatsby manifest plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -66,6 +66,23 @@ module.exports = {
         },
         pages: []
       }
+    }, {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Izac Cavalheiro",
+        short_name: "Izac Cavalheiro",
+        start_url: "/",
+        background_color: "#000",
+        theme_color: "#000",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "src/presentation/assets/images/logo-izac-cavalheiro.png", // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: `use-credentials`
+      }
     }
   ]
 }
