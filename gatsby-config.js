550e34e2e5c8ed15d2aee7f550a7882a52d94381
izaccavalheiro <izac@izaccavalheiro.com>
2021-08-12 01:09:36 -0300

    Update manifest logo address

--- gatsby-config.js
+++ gatsby-config.js
@@ -77,19 +77,19 @@ module.exports = {
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
         display: "standalone",
-        icon: "src/presentation/assets/images/logo-izac-cavalheiro.png", // This path is relative to the root of the site.
+        icon: "icons/icon-256x256.png", // This path is relative to the root of the site.
         // An optional attribute which provides support for CORS check.
         // If you do not provide a crossOrigin option, it will skip CORS for manifest.
         // Any invalid keyword or empty string defaults to `anonymous`
         crossOrigin: `use-credentials`,
         icons: [{
-          "src": "src/presentation/assets/images/logo-izac-cavalheiro.png",
+          "src": "icons/icon-512x512.png",
           "sizes": "512x512",
           "type": "image/png",
           "purpose": "any maskable"
         }, {
-          "src": "src/presentation/assets/images/logo-izac-cavalheiro-196.png",
-          "sizes": "196x196",
+          "src": "icons/icon-192x192.png",
+          "sizes": "192x192",
           "type": "image/png",
           "purpose": "any maskable"
         }]
