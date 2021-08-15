67b4552aadd83be9a7b29e6086b841be71d02e1d
izaccavalheiro <izac@izaccavalheiro.com>
2021-08-15 12:02:25 -0300

    Fix Typography component key Fix Logo universal breakpoint

--- src/presentation/modules/Logo/styles.js
+++ src/presentation/modules/Logo/styles.js
@@ -1,28 +1,26 @@
 import styled from 'styled-components'
 
 export const Wrapper = styled.a`
-  @media(min-width: 0rem) {
-    ${props => props.size === 'large' && (
-      `max-width: 100%;
-      min-width: 100%;
-      max-height: unset;
-      min-height: unset;`
-    )}
+  ${props => props.size === 'large' && (
+    `max-width: 100%;
+    min-width: 100%;
+    max-height: unset;
+    min-height: unset;`
+  )}
 
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  > img {
     flex: 1;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-
-    > img {
-      flex: 1;
-      min-width: 100%;
-      max-width: 100%;
-      width: 100%;
-      height: auto;
-      min-height: auto;
-      max-height: unset;
-    }
+    min-width: 100%;
+    max-width: 100%;
+    width: 100%;
+    height: auto;
+    min-height: auto;
+    max-height: unset;
   }
 
   @media(min-width: 20rem) {
--- src/presentation/modules/Typography/Typography.tsx
+++ src/presentation/modules/Typography/Typography.tsx
@@ -6,37 +6,40 @@ import {
   H2
 } from "../../components"
 
-export const Typography = ({text, tag = 'p'}) => {
-  const Element = ({children, tag}) => {
-    if (tag === 'p') {
-      return <Paragraph>{children}</Paragraph>
-    }
-
-    if (tag === 'h1') {
-      return <H1>{children}</H1>
-    }
-
-    if (tag === 'h2') {
-      return <H2>{children}</H2>
-    }
+const Element = ({children, tag}) => {
+  if (tag === 'p') {
+    return <Paragraph>{children}</Paragraph>
   }
 
-  if (text.indexOf('\r\n') >= 0) {
-    const textParts = text.split('\r\n')
+  if (tag === 'h1') {
+    return <H1>{children}</H1>
+  }
 
-    return textParts && textParts.filter(textPartItem => !!textPartItem).map((textPartItem) => {
-      return <Element tag={tag}>{textPartItem}</Element>
+  if (tag === 'h2') {
+    return <H2>{children}</H2>
+  }
+}
+
+export const Typography = ({text, tag = 'p'}) => {
+  const rn = '\r\n'
+  const n = '\n'
+  const linebreak = text.search(rn) ? rn : n
+  const textParts = text.split(linebreak)
+
+  if (linebreak === '\r\n') {
+    return textParts && textParts.filter(textPartItem => !!textPartItem).map((textPartItem, index) => {
+      const key = `typography-element-${tag}-${index}`
+
+      return <Element tag={tag} key={key}>{textPartItem}</Element>
     })
   }
 
-  if (text.indexOf('\n') >= 0) {
-    const textParts = text.split('\n')
-
-    const ParagraphBr = textParts && textParts.filter(textPartItem => !!textPartItem).map((textPartItem, index) => {
+  if (linebreak === '\n') {
+    const children = textParts && textParts.filter(textPartItem => !!textPartItem).map((textPartItem, index) => {
       return <>{textPartItem} {index < textParts.length - 1 ? <br /> : null}</>
     })
 
-    return <Element tag={tag}>{ParagraphBr}</Element>
+    return <Element tag={tag}>{children}</Element>
   }
 
   return <Element tag={tag}>{text}</Element>
