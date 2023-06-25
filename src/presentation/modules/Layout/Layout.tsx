8365505727befb4ba726f2973cff36ced6c177ec
Izac Cavalheiro <izaccavalheiro@Izacs-MacBook-Air.local>
2023-06-25 02:59:37 -0300

    feat: add defer attribute for ic-webcomponents urls

--- src/presentation/modules/Layout/Layout.tsx
+++ src/presentation/modules/Layout/Layout.tsx
@@ -45,8 +45,8 @@ const LayoutComponent = (props: LayoutProps): React.ReactElement => {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet" />
 
-        <script src="https://unpkg.com/@webcomponents/webcomponentsjs@2.4.3/webcomponents-loader.js"></script>
-        <script type="module"
+        <script defer src="https://unpkg.com/@webcomponents/webcomponentsjs@2.4.3/webcomponents-loader.js"></script>
+        <script defer type="module"
           async
           src="https://unpkg.com/ic-webcomponents@0.0.22/dist/ic-webcomponents/ic-webcomponents.esm.js"
         ></script>
